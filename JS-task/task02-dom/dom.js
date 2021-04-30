9ac052db2d7a9ce1b957f4c07c8a41ea5456d7ad
Arnold <antaloczi.arnold@gmail.com>
2021-04-30 11:00:22 +0200

    Completed task02

--- JS-task/task02-dom/dom.js
+++ JS-task/task02-dom/dom.js
@@ -1,4 +1,21 @@
-function changeOuterLinks() {}
+function changeOuterLinks() {
+  const navElement = document.querySelector('#link-list');
+  const links = document.querySelectorAll('#link-list a');
 
+  links.forEach(link => {
+    if (link.innerHTML.includes('outer-link')) {
+      link.setAttribute('target', '_blank');
+      link.innerHTML = `<strong>${link.innerHTML}</strong>`;
+    }
+    return link;
+  });
+
+  navElement.style.display = 'flex';
+  navElement.style.flexDirection = 'column';
+  navElement.style.width = '30%';
+  navElement.style.margin = '0 auto';
+  navElement.style.border = '1px solid blue';
+  navElement.style.padding = '16px';
+}
 
 export { changeOuterLinks };
